b9d80cbd801cd1acb721e54a61e71e21a8e69169
TownECreator <gamesinchinatown@gmail.com>
2026-03-20 18:05:57 +0000

    comments abt armor

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,4 +2,4 @@ uname -a #da info sobre el ordenador
 which gpg #ubicación del binario de gpg
 gpg --version #verifica que todo esté instalado correctamente
 gpg --full-generate-key #crear un nuevo par de claves criptográficas 
-gpg --armor --export
+gpg --armor --export #exporta la llave pública
