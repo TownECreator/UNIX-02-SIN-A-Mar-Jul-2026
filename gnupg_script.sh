94f2165ed6abe1ce5317b9fb8ef7b10d62b9c3d0
TownECreator <gamesinchinatown@gmail.com>
2026-03-26 13:59:27 +0000

    docs: fix minor spelling errors

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -9,7 +9,7 @@ gpg --list-keys #para mostrar llaves existentes
 #para exportar una llave a un documento: gpg --armor --export usuario@ejemplo.com > mi_clave_publica.asc
 gpg --list-secret-keys --keyid-format-long #exportar la llave privada
 gpg --armor --export-secret-keys XXXXXXX #exporta una llave privada específica reemplazando las X por un sec específico, no es necesario las XXX si solo se tiene 1 par de llaves
-gpg --armor --export correo1@gmail.com #exsportar
+gpg --armor --export correo1@gmail.com #exportar
 gpg --import NOMBRELEGIDO_mi_llave_públic.asc #lo ultimo depende del nombre, es importar la llave de alguien mas, es necesario que esté importado el archivo de la llave primero
 $ echo "este mensaje es solo para sus ojos" > doc_no_cifrado.txt #creacion de un mensaje estandar
 gpg --output doc_cifrado.txt --encrypt --recipient slucina0406@gmail.com doc_no_cifrado.txt #esto es para enviar un documento cifrado a la persona de la que tenemos la info de la llave
