f353d8ca291f285da07d132f4e472878f8497201
TownECreator <gamesinchinatown@gmail.com>
2026-03-27 17:09:49 +0000

    chore: compile saludo.c into binary and install it in /bin for global execution
    - compiled C source file using gcc to generate "saludobin"
    - moved binary to /bin with superuser privileges
    - verified installation by listing /bin/saludobin
    - executed binary to confirm correct behavior

--- Sistemas de Ficheros/filesystem_script.sh
+++
@@ -1,8 +0,0 @@
-#change directory to /dev directory
-cd /dev
-#list the files and directories in the /dev directory
-ls
-#change directory to /proc
-cd /proc #Each number = a running process
-#Shows information about the processor
-cat cpuinfo 
---
+++ saludo.c
@@ -0,0 +1,7 @@
+//Standard Input/Output = stdio.h
+#include <stdio.h>
+
+int main() {
+    printf("¡Hola, mundo!\n");
+    return 0; //means that the program ended successfully
+}
