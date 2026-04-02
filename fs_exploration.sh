2ffebc51aa9c4d95668905be64aea0da2006b253
TownECreator <gamesinchinatown@gmail.com>
2026-04-02 12:48:50 +0000

    chore: run apt update, upgrade system, and install parted

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -1,2 +1,13 @@
 cd / #changes the directory to the root directory of the filesystem.
 ls -F #list files and directories, adding a symbol to show their type.
+#we have both libs (normal and 64) because of compatibility
+#the first thing that happens when we start a computer is the POST
+#THE INITRD.ING is a file used during the boot process to temporarily load essential drivers and tools into memory before the main system starts.
+# THE VMLINUZ.OLD its the backup of the Linux kernel
+#FHS
+#carga de firmware, the bootloader, then kernel
+sudo apt update #Updates the list of available packages from the repositories.
+sudo apt upgrade #Upgrades all installed packages to their latest available versions.
+sudo apt install parted #Installs the parted program on your system.
+
+#sometimes you can turn up with a dependencies hell haha
