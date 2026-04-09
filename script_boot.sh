9a8fbfd1a7f7c0580287fc30344c3fcf27b2f398
TownECreator <gamesinchinatown@gmail.com>
2026-04-09 12:37:40 +0000

    chore: environment variables and system navigation
    - echo $HOME: prints the user's home directory path
    - echo $BASH: prints the Bash shell path
    - echo $PATH: shows directories where commands are located
    - cd /usr/local: changes directory to system software folder

--- script_boot.sh
+++ script_boot.sh
@@ -6,3 +6,17 @@ ls # Lists the files and directories in the current folder
 cd proyecto/ # Moves into the "proyecto" directory
 cd .. # Goes up one level to the parent directory
 cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto # Moves to this directory using an absolute path (from the root)
+cd #change directory to home directory
+pwd # Prints the current working directory (shows where you are)
+cd / # Changes directory to the root folder (/) 
+#it does not matter what directory you are in, if u put cd it will move you into that
+cd /workspaces/   # Changes directory to the /workspaces folder using an absolute path
+# this ~ identifies the home directory
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/   # Changes directory to this specific project folder using an absolute path
+cd /home/codespace/   # Changes directory to the codespace user's home directory using an absolute path
+#Environment variables are values stored in the operating system that programs can use to configure their behavior.
+#the most basic thing you can do is write something in one archive and make another one read it
+echo $HOME   # Prints the user's home directory path (environment variable)
+echo $BASH   # Prints the path to the Bash shell executable
+echo $PATH   # Prints the list of directories where the system looks for executable commands
+cd /usr/local   # Changes directory to /usr/local, a system folder where locally installed software is stored
