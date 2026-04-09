25f61425f492cc5472685e085d3b3b869243b5e2
TownECreator <gamesinchinatown@gmail.com>
2026-04-09 12:24:40 +0000

    chore: set up base project structure

---
+++ script_boot.sh
@@ -0,0 +1,8 @@
+git checkout branch name #is used to switch to another branch in your Git repository.
+#An absolute route (or absolute path) is the full path to a file or directory starting from the root of the file system, no matter where you currently are.
+#A relative path (or relative route) is a path that is defined based on your current location (current directory), not from the root.
+mkdir proyecto # Creates a new directory (folder) named "proyecto"
+ls # Lists the files and directories in the current folder
+cd proyecto/ # Moves into the "proyecto" directory
+cd .. # Goes up one level to the parent directory
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto # Moves to this directory using an absolute path (from the root)
