28d14144cb83ba19474fbc4cba90b33556cf5b7d
TownECreator <gamesinchinatown@gmail.com>
2026-04-10 17:22:57 +0000

    Linux S.O is running

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # UNIX-02-SIN-A-Mar-Jul-2026
 Repo for intro to UNIX
 ![alt text](imagen.png)
+![alt text](imagen-1.png)
--- script_distro.sh
+++ script_distro.sh
@@ -31,25 +31,69 @@ cd ..
 # Moves one directory up (goes back to the parent directory, exiting /linux)
 
 git clone --depth 1 https://git.busybox.net/busybox
-cs -t fat boot
-syslinux boot
-mkdir m
-mount boot m
-cp bzImage init.cpio m
-umount md busybox
+# Clones a shallow copy (latest version only) of the BusyBox source code
+
+cd busybox
+# Enters the BusyBox source directory
+
 make menuconfig
+# Opens a configuration menu to select BusyBox features
+
 make -j 2
+# Compiles BusyBox using 2 parallel jobs
+
 sudo mkdir /boot-files/initramfs
+# Creates the directory that will act as the initramfs root filesystem
+
 sudo make CONFIG_PREFIX=/boot-files/initramfs install
+# Installs BusyBox into the initramfs directory structure
+
 cd /boot-files/initramfs
+# Moves into the initramfs directory
+
 sudo vi init
+# Creates or edits the init file (first program executed by the kernel)
+
 #! /bin/sh
 /bin/sh
-sudo rm linuxrc
-sudo chmod +x init
-sudo sh -c "find . | cpio -o -H newc > ../init.cpio"
-cd
+# Minimal init script that launches a shell
 
-sudo su 
+sudo rm linuxrc
+# Removes the default linuxrc file to avoid conflicts
+
+sudo chmod +x init
+# Makes the init script executable
+
+sudo sh -c "find . | cpio -o -H newc > ../init.cpio"
+# Creates the initramfs archive (init.cpio) from current directory
+
+cd
+# Returns to the home directory
+
+sudo su
+# Switches to root user
+
 dd if=/dev/zero of=boot bs=1M count=50
-mkf
+# Creates a 50MB empty file to be used as a virtual disk
+
+mkfs -t fat boot
+# Formats the file as a FAT filesystem
+
+syslinux boot
+# Installs the Syslinux bootloader into the disk image
+
+mkdir m
+# Creates a mount point directory
+
+mount boot m
+# Mounts the disk image into directory m
+
+cp bzImage init.cpio m
+# Copies the kernel and initramfs into the mounted disk
+
+umount m
+# Unmounts the disk image
+
+qemu-system-x86_64 -nographic -append "console=ttyS0" \
+-kernel bzImage -initrd init.cpio -drive file=boot,format=raw
+# Starts QEMU without GUI, using serial console, loading kernel and initramfs
