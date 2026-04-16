ec88d3688c198a8abbad260491857a21186526af
TownECreator <gamesinchinatown@gmail.com>
2026-04-16 13:09:47 +0000

    docs: explore git manual pages and understand shallow clone with --depth

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -18,3 +18,14 @@ mkdir -- -rf
 rm -rf #forcefully and recursively deletes files and directories without confirmation (very dangerous) usually it does not work with directories with '-' 
 
 rmdir -- -rf #removes an empty directory named "-rf" by using -- to stop option parsing, it works with direcories starting with '-'
+
+ls --help #displays a quick summary of ls command options and usage in the terminal
+
+man ls #opens the full manual page for ls with detailed documentation (navigation with N for fast rewind, n for fast foward, exit with 'q', search with '/')
+
+man git #opens the main Git manual page with an overview of the system, common commands, and general usage (press 'q' to exit)
+
+man git-clone #opens the manual page for the git clone command, showing detailed usage, options, and examples (press 'q' to exit)
+
+--depth # Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless --no-single-branch is given to fetch the histories near the tips of all branches. If you want to clone submodules shallowly, also pass --shallow-submodules.
+    #it creates a shallow copy because with depth, we can choose some commits to copy from
