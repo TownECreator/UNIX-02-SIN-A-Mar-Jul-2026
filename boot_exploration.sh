8c1ccb542433adde39dd69e1e95300c359bf65ff
TownECreator <gamesinchinatown@gmail.com>
2026-04-16 13:47:07 +0000

    docs: add notes on using sudo tee and file permission checks

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -35,16 +35,36 @@ man git-clone #opens the manual page for the git clone command, showing detailed
 
 chmod #means change mode, it has two sintaxes, one symbolic and one numeric
 
-touch script.sh            # creates an empty file named script.sh
+touch script.sh #creates an empty file named script.sh
 
-chmod +x script.sh         # adds execute permission for all users (user, group, others)
+chmod +x script.sh #adds execute permission for all users (user, group, others)
 
-chmod u+x script.sh        # adds execute permission only for the owner (user)
+chmod u+x script.sh #adds execute permission only for the owner (user)
 
-touch secreto.txt          # creates an empty file named secreto.txt
+touch secreto.txt #creates an empty file named secreto.txt
 
-chmod o-r secreto.txt      # removes read permission for others (public)
+chmod o-r secreto.txt #removes read permission for others (public)
 
-touch privado              # creates an empty file named privado
+touch privado #creates an empty file named privado
 
-chmod u+rw,go-rwx privado  # gives read/write to owner, removes all permissions from group and others
+chmod u+rw,go-rwx privado #gives read/write to owner, removes all permissions from group and others
+
+sudo chmod +x init # init is the first process that the linux model executes, we have in the init a shell waiting to write what do we want to execute, in some cases, bash
+#comprobamos q esta hecho con ls -l
+Commands like
+sudo echo "hola" > /etc/archivo_protegido 
+#does NOT work because 'sudo' only applies to echo, not to the redirection '>'
+#the '>' is handled by the shell without sudo, so it fails due to permission denied
+
+#/etc = carpeta donde se guardan configuraciones globales del sistema y de programas
+
+echo "hola" | sudo tee /etc/archivo_protegido #this works because tee runs with sudo and writes to the protected file
+
+echo "hola" | sudo tee /etc/archivo_protegido > /dev/null #writes “hola” into a protected file using sudo, without showing output. If we were to erase this /dev/null, it would definitely show the info inside.
+
+ls -l /etc/archivo_protegido or as well ls /etc #shows file details like permissions, owner, and size
+
+cat /etc/archivo_protegido #displays the file’s content (if you have permission)
+
+echo "hola" | sudo tee /etc/archivo_protegido #writes “hola” into a protected file using sudo and also prints “hola” on the screen
+#both of these are absolute routes (cat and ls)
