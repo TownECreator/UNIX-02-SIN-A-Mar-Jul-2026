253530d13b573977f0fc61599a1314d5aecc874e
TownECreator <gamesinchinatown@gmail.com>
2026-04-16 12:49:29 +0000

    feat: demonstrate usage of ls command with common flags and combinations

---
+++ boot_exploration.sh
@@ -0,0 +1,10 @@
+ls -a #shows all files, includding hidden ones (those starting with .)
+
+ls --all #Does the same as -a, it's just the long form version of the flag
+
+ls -l #Displays files in long format (permissions, owner, size, date, filename, etc)
+
+ls -h #shows the file sizes in human-readable format
+
+ls -l -a -h #combines everything the previous ls do
+ls -lah # Same as above, just the short combined version of the flags
