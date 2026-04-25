f7a56a74f6a8cc59968cd486fd7ba58ead5c77bf
TownECreator <gamesinchinatown@gmail.com>
2026-04-25 22:12:01 +0000

    LinuxUnhatched: Used ls to list contents of Documents directory

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,1 +1,2 @@
 ls #lists files and folders in the current directory (or a specified path), with options like -l or -a for more details
+ls Documents # Lists the contents of the "Documents" directory, in this case, since the 'Documents' directory does not exist, it simply gives us an error
