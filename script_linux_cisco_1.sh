16bad3ae23cc97e5335bee359bce0b4f6c1c7bbd
TownECreator <gamesinchinatown@gmail.com>
2026-04-25 22:16:56 +0000

    LinuxUnhatched: Explained aptitude moo (a easter egg) and how to install aptitude in Codespaces

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,2 +1,4 @@
 ls #lists files and folders in the current directory (or a specified path), with options like -l or -a for more details
 ls Documents # Lists the contents of the "Documents" directory, in this case, since the 'Documents' directory does not exist, it simply gives us an error
+aptitude moo # 'aptitude moo' is a fun Easter egg that prints an ASCII cow; it requires the 'aptitude' package,
+# which isn’t installed by default in Codespaces, so we'll need to run 'sudo apt update && sudo apt install aptitude' first. In this case, we hace no easter eggs in the program.
