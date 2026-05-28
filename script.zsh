86b74b4e21c6a5aff16af063b991452752245ec2
TownECreator <gamesinchinatown@gmail.com>
2026-05-28 12:54:27 +0000

    docs: document current group verification and pre-newgrp file test

--- script.zsh
+++ script.zsh
@@ -5,3 +5,12 @@ id -gn #only the name of the primary group
 touch ~/test_grupo_heredado.txt
 ls -la ~/test_grupo_heredado.txt
 #the group is the primary group of the user
+
+
+#Look for current group
+id -gn
+echo "Grupo actual: $(id -gn)"
+
+#Create a file before newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
