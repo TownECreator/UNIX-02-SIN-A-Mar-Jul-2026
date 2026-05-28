cc271be59597cae7cb6a27dc6bd5be49244857d4
TownECreator <gamesinchinatown@gmail.com>
2026-05-28 13:45:38 +0000

    test(groups): verify restricted group access using new external user
    * Created a new user outside the restricted group
    * Switched to the new user account
    * Tested `newgrp grupo_restringido`
    * Verified group password authentication behavior

--- script.zsh
+++ script.zsh
@@ -40,5 +40,23 @@ exit
 id -gn
 echo "grupo restaurado después de exit: $(id -gn)"
 
+
 #Compare the groups of the files created before and inside newgrp
 ls -la ~/antes_de_newgrp.txt ~/dentro_de_newgrp.txt
+
+#newgrp creates a new subshell with the new group, so the files created inside it will have the new group as their group, while the files created before will have the primary group of the user.
+echo "PID del proceso actual: $$"
+
+#Create a group with password
+sudo (only if u are not root) groupadd grupo_restringido
+sudo (only if u are not root) gpasswd grupo_restringido
+#the system will ask for a password for the group, and then it will ask to confirm it. After that, the group will be created with the password.
+
+
+#create a new user not inside the group
+sudo useradd -m usuario1
+#Try to change to the new group with the new user
+su - usuario1
+newgrp grupo_restringido
+#The system will ask for the password of the group, and if the password is correct
+#but first we need to create a user outside the group and then try to change to the new group with that user, because if we are already inside the group, we won't be able to test the password restriction.
