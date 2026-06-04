aeeb71c666cc3c1af4ed3de3458af34280026860
TownECreator <gamesinchinatown@gmail.com>
2026-06-04 13:01:44 +0000

    feat: add environment diagnostic script
    - Include core shell variable checks (SHELL, UID, OSTYPE)
    - Enable xtrace debugging with set -x
    - Add process (ps) and disk usage (df) snapshot commands

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
+set -x
 bash --version
 env
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+
+#man ls 
+#ls -l
+#ls --help
+#ls -a
+#ls -all
+ps -ef
+df --human-readable
+#bash -r blackhatbash1.sh
+#bash -n blackhatbash1.sh
+#bash -x blackhatbash1.sh
+
+set +x
