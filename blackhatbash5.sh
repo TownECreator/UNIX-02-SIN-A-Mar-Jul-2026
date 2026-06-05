05b3562b40e22e1175dbb9c17d7e85611e81be2b
TownECreator <gamesinchinatown@gmail.com>
2026-06-05 17:16:17 +0000

    docs: document bash control operators and file redirection tests
    - Added control operators tests (&&, ||, and sequential semi-colons)
    - Added output redirection examples using > and >>
    - Documented file descriptor splitting for stdout (1>) and stderr (2>)

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,3 +3,21 @@ top # Displays real-time information about running processes, CPU usage, memory 
 top & # Runs the 'top' command in the background, allowing you to continue using the terminal while 'top' keeps running. 
 kill -9 process ID # Forcefully terminates the process with the specified Process ID (PID) using the SIGKILL signal.
 kill -9 7843 # Forcefully terminates the process whose PID is 7843.
+
+
+touch test && touch test123
+(ls; ps)
+ls; ps; whoami
+lzl || echo "El comando lzl ha fallado"
+echo "Hola mundo" > output1.txt
+cat output1.txt
+echo "chao mundo" > output1.txt
+echo "chao chao" >> output1.txt
+cat output1.txt
+
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / &>> stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+lzl 2> error.txt
+cat error.txt
