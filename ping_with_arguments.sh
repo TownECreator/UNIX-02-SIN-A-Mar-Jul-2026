bb222f17c488d92d2f0ce7584c6b130fdddf5106
TownECreator <gamesinchinatown@gmail.com>
2026-06-09 16:39:45 +0000

    feat: limit ping count to 5 transmissions
    Restricted the ping command to execution with a maximum of 5 counts
    using the '-c' flag to prevent infinite loops.

---
+++ ping_with_arguments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script will ping any address provided as an argument.
+SCRIPT_NAME="${0}"
+TARGET="${1}"
+echo "Running the script ${SCRIPT_NAME}..."
+echo "Pinging the target: ${TARGET}..."
+ping -c 5 "${TARGET}"
+
+#to execute it use:
+#chmod u+x ping_with_arguments.sh 
+#./ping_with_arguments.sh nostarch.com
+#-c 5 is to ONLY ping 5 times
+#the order 127.0.0.1 is in $1, and is saved in TARGET
