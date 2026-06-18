ad0eff20c465b55cc308a802136d7dc6a6c24f22
TownECreator <gamesinchinatown@gmail.com>
2026-06-18 12:52:58 +0000

    feat: add awk parsing commands for log analysis

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -13,3 +13,13 @@ ps | grep -i tty
 grep -v "35.237.4.214" log.txt
 
 grep -o "35.237.4.214" log.txt
+
+awk '{print $1}' log.txt
+
+#case apart, we recommend using apache parquet
+
+awk '{print $1,$2,$3}' log.txt
+
+awk '{print $2}' log.txt
+
+awk '{print $3}' log.txt
