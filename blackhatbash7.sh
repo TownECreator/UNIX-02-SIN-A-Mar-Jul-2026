7792feb9465b873e2c8f568346c995ab505dc637
TownECreator <gamesinchinatown@gmail.com>
2026-06-18 12:38:10 +0000

    Add grep scripts to extract metrics from firewall log

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+grep "35.237.4.214" log.txt
+
+grep "35.237.4.214\|13.66.139.0" log.txt #backslash signifies scape character, can be replaced by -e
+
+grep -e "35.237.4.214" -e "13.66.139.0" log.txt
+
+
