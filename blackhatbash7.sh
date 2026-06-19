428b9047d30149e1f8dc865d9ae8b9d8c28c4c20
TownECreator <gamesinchinatown@gmail.com>
2026-06-19 16:45:14 +0000

    Replace 'Mozilla' with 'Godzilla' in log files

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -40,3 +40,10 @@ awk 'NR < 10' log.txt
 grep "42.236.10.117" log.txt
 awk '{print $7}' log.txt
 grep "42.236.10.117" log.txt | awk '{print $7}'
+
+
+sed 's/Mozilla/Godzilla/g' log.txt
+
+sed 's/Mozilla/Godzilla/g' log.txt > new_log.txt
+
+grep "Godzilla" new_log.txt
